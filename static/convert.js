4d320146ca077208bbf5f758bfbee18f33225f0f
Jason Michalski <armooo@armooo.net>
2010-11-29 09:30:50 -0500

    Change the conversion UI to output a link

--- static/convert.js
+++ static/convert.js
@@ -2,7 +2,8 @@ function convert_address() {
     var address = $('#address').val()
     $.get('/convert/' + address,
         function (data) {
-            $('#4lw').val(data['4lw'])
+            $('#4lw').attr('href', 'http://' + data['4lw'])
+            $('#4lw').html('http://' + data['4lw'])
         }
     )
 }
